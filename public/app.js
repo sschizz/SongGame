a365b73e5ca4380f02cc21119308ef857ade460d
Son Artyom <sai@modumlab.com>
2022-11-13 14:26:41 +0300

    updates

--- public/app.js
+++ public/app.js
@@ -1,25 +1,56 @@
-const fs = require('fs')
-const http = require('http')
+const SERVER = "http:/localhost:3000";
+const SONG_ID = "79fzeNUqQbQ";
 
+const getAudio = (id) => {
+  return new Promise((resolve, reject) => {
+    var context = new AudioContext();
+    var request = new XMLHttpRequest();
+    request.open("GET", `/song/${id}`, true);
+    request.responseType = "arraybuffer";
+    request.onload = () => {
+      if (request.status == 200) {
+        resolve(request.response);
+      } else {
+        reject(request.status);
+      }
+    };
+    request.send();
+  });
+};
 
-document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.getElementById('btn_search')
-    const input = document.getElementById('input')
-    const socket = io()
+const ctx = new AudioContext();
+let audio;
 
-    btn.addEventListener('click', async () => {
-        socket.emit('song-search', input.value)
-        document.getElementById('audio').play()
-    })
+function PlayBack() {
+  const play_sound = ctx.createBufferSource();
+  play_sound.buffer = audio;
+  play_sound.connect(ctx.destination);
+  play_sound.start(ctx.currentTime);
+}
 
-    const audio = document.getElementById("audio")
-    var res = fs.createWriteStream(audio)
-    console.log(res)
-    socket.on('search-result', (r) => {
-        console.log(r)
-    })
+window.addEventListener("load", async () => {
+  const btn = document.getElementById("btn_search");
+  const input = document.getElementById("input");
 
-    socket.on('new-connection', id => {
-        console.log(`Welcome ${id}`)
-    })
-})
+  btn.addEventListener("click", async () => {
+    socket.emit("song-search", input.value);
+
+    await fetch(`/song/${SONG_ID}`)
+      .then((data) => data.arrayBuffer())
+      .then((arrayBuffer) => ctx.decodeAudioData(arrayBuffer))
+      .then((decoded_audio) => {
+        audio = decoded_audio;
+      });
+
+    PlayBack();
+  });
+
+  const socket = io();
+  socket.on("search-result", (r) => {
+    console.log(r);
+  });
+
+  socket.on("new-connection", (id) => {
+    console.log(`Welcome ${id}`);
+  });
+});
--- server.js
+++ server.js
@@ -1,41 +1,52 @@
-const yts = require('yt-search')
-const stream = require('youtube-audio-stream')
-const { Writable } = require('stream')
-const fs = require('fs')
+const yts = require("yt-search");
+const stream = require("youtube-audio-stream");
 
-const express = require('express')
-const http = require('http')
-const path = require('path')
-const PORT = process.env.PORT || 3000
-const socketio = require('socket.io')
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const socketio = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.get('/song', (req, res) => {
-    stream('http://www.youtube.com/watch?v=rtL5oMyBHPs').pipe(res)
-})
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-app.put('/song', (req, res) => {
+app.use(cors());
 
-})
+app.get("/song/:id", cors(corsOptions), (req, res) => {
+  stream(`https://www.youtube.com/watch?v=${req.params.id}`).pipe(res);
+});
 
-app.use(express.static(path.join(__dirname, "public")))
-server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
+app.get("/test", cors(corsOptions), (req, res) => {
+  return res.json({msg: "test"});
+});
 
-io.on('connection', socket => {
-    console.log(`New connection ${socket.id}`)
-    socket.emit('new-connection', socket.id)
+app.get("/message/:id", cors(corsOptions), (req, res) => {
+  console.log(`${req.params.id}`);
+  return res.json({ msg: `${req.params.id}` });
+});
 
-    socket.on('song-search', async val => {
-        if (String(val) <= 0)
-            return
+io.on("connection", (socket) => {
+  console.log(`New connection ${socket.id}`);
+  socket.emit("new-connection", socket.id);
 
-        var r = await yts(String(val))
-        var res = fs.createWriteStream('./output')
-        for await (const chunk of stream(r.videos[0].url)) {
-            res.write(chunk)
-        }
-        res.end()
-    })
-})
+  socket.on("song-search", async (val) => {
+    if (String(val) <= 0) return;
+
+    var r = await yts(String(val));
+    var res;
+    for await (const chunk of stream(r.videos[0].url)) {
+      res.write(chunk);
+    }
+    res.end();
+    return res;
+  });
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
